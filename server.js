a8ab3f6eef8265c16ed6cb398d1405b136c71faf
5a9ar <sagarluitel01>
2018-02-22 12:49:40 -0700

    new update.1

--- server.js
+++ server.js
@@ -27,6 +27,19 @@ router.route('/post')
         }
     );
 
+router.route('/get')
+    .get(authController.isAuthenticated, function (req, res) {
+            console.log(req.body);
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                console.log("Content-Type: " + req.get('Content-Type'));
+                res = res.type(req.get('Content-Type'));
+            }
+            res.send("Empty data");
+        }
+    );
+
+
 router.route('/postjwt')
     .post(authJwtController.isAuthenticated, function (req, res) {
             console.log(req.body);
